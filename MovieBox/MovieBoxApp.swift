ecc3e1aeb053ba52acdf2c8e2e2046506d0b4e18
Yasin Onur <onur23@itu.edu.tr>
2025-05-22 16:19:55 +0300

    Initial Commit

---
+++ MovieBox/MovieBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBoxApp.swift
+//  MovieBox
+//
+//  Created by Yasin Onur on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
