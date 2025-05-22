0a29d0c70524fe80531428ab1db87d66810361b6
Yasin Onur <onur23@itu.edu.tr>
2025-05-22 17:13:09 +0300

    Add User model to store user information

---
+++ MovieBox/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MovieBox
+//
+//  Created by Yasin Onur on 22.05.2025.
+//
+
+
+struct User: Decodable {
+    let id: String
+    let name: String
+    let surname: String
+    let email: String
+}
