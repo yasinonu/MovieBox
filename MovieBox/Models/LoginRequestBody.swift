4c4e44f4fa5810898295318d6904b69bd3e6fc6c
Yasin Onur <onur23@itu.edu.tr>
2025-05-22 18:42:52 +0300

    Add LoginRequestBody model for login API

---
+++ MovieBox/Models/LoginRequestBody.swift
@@ -0,0 +1,11 @@
+//
+//  LoginRequestBody.swift
+//  MovieBox
+//
+//  Created by Yasin Onur on 22.05.2025.
+//
+
+struct LoginRequestBody: Encodable {
+    let email: String
+    let password: String
+}
