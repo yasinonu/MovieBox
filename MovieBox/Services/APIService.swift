3419e12653276178736037a380b538ee1b494722
Yasin Onur <onur23@itu.edu.tr>
2025-05-22 16:42:47 +0300

    Add APIService to perform API calls

---
+++ MovieBox/Services/APIService.swift
@@ -0,0 +1,15 @@
+//
+//  APIService.swift
+//  MovieBox
+//
+//  Created by Yasin Onur on 22.05.2025.
+//
+
+import Foundation
+
+
+struct APIService {
+    let baseURL: URL = URL(string: "https://moviatask.cerasus.app")!
+    
+    
+}
