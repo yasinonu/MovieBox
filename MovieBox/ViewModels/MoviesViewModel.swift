d422ea67fe8242a7da996c9b9abf3e275e967dce
Yasin Onur <onur23@itu.edu.tr>
2025-05-23 18:05:42 +0300

    Add fetchMovies to retreive all movies

--- MovieBox/ViewModels/MoviesViewModel.swift
+++ MovieBox/ViewModels/MoviesViewModel.swift
@@ -9,4 +9,19 @@ import Foundation
 
 class MoviesViewModel: ObservableObject {
     private let apiService: APIService = .shared
+    
+    // Movies
+    @Published var movies: [Movie] = []
+    
+    // Fetch Movies
+    public func fetchMovies() async {
+        do {
+            let movies = try await apiService.fetchAllMovies()
+            
+            self.movies = movies
+        }
+        catch {
+            print("Error fetching movies: \(error)")
+        }
+    }
 }
