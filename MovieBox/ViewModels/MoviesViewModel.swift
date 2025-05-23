545c49e5d01735db426f0546a3bf472cdc28fa85
Yasin Onur <onur23@itu.edu.tr>
2025-05-23 18:02:24 +0300

    Add MoviesViewModel to handle movies state

---
+++ MovieBox/ViewModels/MoviesViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesViewModel.swift
+//  MovieBox
+//
+//  Created by Yasin Onur on 23.05.2025.
+//
+
+import Foundation
+
+class MoviesViewModel: ObservableObject {
+    private let apiService: APIService = .shared
+}
